4e74ab477d378845f6d671058a29a1966c5eb4d2
enigbe <ochekliyeenigbe@gmail.com>
2022-03-26 23:11:43 +0100

    modify knex configuration file

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,27 +1,62 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
-    development: {
-        client: 'postgresql',
-        connection: {
-          database: process.env.databaseName,
-          username: process.env.databaseUsername,
-          password: process.env.databasePassword
-        },
-        pool: {
-          min: 2,
-          max: 10,
-        },
+
+  development: {
+    client: 'postgresql',
+    connection: {
+        host: process.env.POSTGRESQL_HOST,
+        port: process.env.POSTGRESQL_PORT,
+        user: process.env.POSTGRESQL_USER,
+        password: process.env.POSTGRESQL_PASSWORD,
+        database: process.env.POSTGRESQL_DATABASE_NAME
     },
-  
-    production: {
-      client: 'postgresql',
-      connection: {
-        database: process.env.databaseName,
-        username: process.env.databaseUsername,
-        password: process.env.databasePassword
-      },
-      pool: {
+    pool: {
         min: 2,
-        max: 10,
+        max: 10
       },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+        host: process.env.POSTGRESQL_HOST,
+        port: process.env.POSTGRESQL_PORT,
+        user: process.env.POSTGRESQL_USER,
+        password: process.env.POSTGRESQL_PASSWORD,
+        database: process.env.POSTGRESQL_DATABASE_NAME
     },
-  };
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+        host: process.env.POSTGRESQL_HOST,
+        port: process.env.POSTGRESQL_PORT,
+        user: process.env.POSTGRESQL_USER,
+        password: process.env.POSTGRESQL_PASSWORD,
+        database: process.env.POSTGRESQL_DATABASE_NAME
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
